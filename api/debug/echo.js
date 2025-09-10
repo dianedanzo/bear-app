3c9f29a8a48a3fc4ca9e4014c865fa200bedc12e
dianedanzo <ahhhhmm97@gmail.com>
2025-09-11 00:54:53 +0700

    Delete api/debug/echo.js

--- api/debug/echo.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res) => {
-  const raw = req.headers["x-init-data"] || req.headers["x-initdata"] || "";
-  res.status(200).json({ hasInitHeader: !!raw, initDataLength: (raw || "").length });
-};
