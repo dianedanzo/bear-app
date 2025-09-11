f034940c742e8bde82f94a92a1cdf234d1583253
dianedanzo <ahhhhmm97@gmail.com>
2025-09-11 21:32:26 +0700

    Update _supabase.js

--- api/_supabase.js
+++ api/_supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-
-export const svc = () =>
-  createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY,
-    { auth: { persistSession: false } }
-  );
+export const svc = () => createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  { auth: { persistSession: false } }
+);
